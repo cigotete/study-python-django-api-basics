f5cf196bc31a96ce37ee6e9d02d62b638bfe853b
Camilo Figueroa <cigotete@gmail.com>
2024-01-25 13:01:28 -0500

    Added in urls.py project level the url prefix for Posts endopoints.

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/todos/", include("todos.urls")),
+    path("api/posts/v1/", include("posts.urls")),
 ]
