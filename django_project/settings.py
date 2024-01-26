a05d6f3d9e6cb003c3ffa3069503883f7e7c32b8
Camilo Figueroa <cigotete@gmail.com>
2024-01-26 14:29:38 -0500

    Updated pagination settings.

--- django_project/settings.py
+++ django_project/settings.py
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 10,
 }
 
 AUTH_USER_MODEL = "accounts.CustomUser"
