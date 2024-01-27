a9cd02ab11b77b2da93f1041c9239175807e05a0
Camilo Figueroa <cigotete@gmail.com>
2024-01-26 19:45:02 -0500

    Removing token as auth system for Post views.

--- posts/views.py
+++ posts/views.py
@@ -4,11 +4,9 @@ from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
-  permission_classes = (IsAuthorOrReadOnly,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-  permission_classes = (IsAuthorOrReadOnly,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
